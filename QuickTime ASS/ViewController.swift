290377d162b26e702d5901f56085f9de62059fa3
xjbeta <xjbeta.tiny@gmail.com>
2021-01-18 12:45:03 +0800

    Initial Commit

---
+++ QuickTime ASS/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  QuickTime ASS
+//
+//  Created by xjbeta on 1/18/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
