99cf0617e87cc04128bfa1ab69942cd1d497de05
xjbeta <xjbeta.tiny@gmail.com>
2021-01-19 18:30:07 +0800

    MainWindowController.

---
+++ QuickTime ASS/MainWindowController.swift
@@ -0,0 +1,18 @@
+//
+//  MainWindowController.swift
+//  QuickTime ASS
+//
+//  Created by xjbeta on 1/19/21.
+//
+
+import Cocoa
+
+class MainWindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+    
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+
+}
