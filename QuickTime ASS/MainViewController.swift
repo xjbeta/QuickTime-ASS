30075efa7e1da41582f191f0f3ef49889b3cf05a
xjbeta <xjbeta.tiny@gmail.com>
2021-01-20 20:39:22 +0800

    MainViewController.

---
+++ QuickTime ASS/MainViewController.swift
@@ -0,0 +1,57 @@
+//
+//  MainViewController.swift
+//  QuickTime ASS
+//
+//  Created by xjbeta on 1/18/21.
+//
+
+import Cocoa
+import Quartz
+
+class MainViewController: NSViewController {
+    
+    @IBOutlet var imageView: IKImageView!
+    
+    let libass = Libass(size: CGSize(width: 1920, height: 1080))
+    
+    let player = QuickTimePlayer()
+    
+    let timer = DispatchSource.makeTimerSource(flags: [], queue: .main)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imageView.backgroundColor = .clear
+        imageView.setImage(nil, imageProperties: nil)
+        
+        NotificationCenter.default.addObserver(forName: .loadNewSubtilte, object: nil, queue: .main) {
+            guard let info = $0.userInfo as? [String: String],
+                  let url = info["url"] else { return }
+            
+            self.libass.setFile(url)
+            self.startTimer()
+        }
+        
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+    func startTimer() {
+        timer.schedule(deadline: .now(), repeating: .milliseconds(250))
+        timer.setEventHandler {
+            self.player.currentTime().done(on: .main) {
+                if let image = self.libass.generateImage(Int64($0)) {
+                    self.imageView.setImage(image, imageProperties: nil)
+                }
+            }.catch {
+                print($0)
+            }
+        }
+        timer.resume()
+    }
+}
+
