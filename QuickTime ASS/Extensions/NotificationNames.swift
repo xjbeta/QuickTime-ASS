b6ef6f5f34ec0ca7fa31ec87fb5f0db352b708eb
xjbeta <xjbeta.tiny@gmail.com>
2021-01-20 20:23:39 +0800

    NotificationName.

---
+++ QuickTime ASS/Extensions/NotificationNames.swift
@@ -0,0 +1,13 @@
+//
+//  NotificationNames.swift
+//  QuickTime ASS
+//
+//  Created by xjbeta on 1/20/21.
+//
+
+import Cocoa
+
+extension Notification.Name {
+    static let loadNewSubtilte = Notification.Name("com.xjbeta.QuickTime ASS.loadNewSubtilte")
+    static let resizeWindow = Notification.Name("com.xjbeta.QuickTime ASS.resizeWindow")
+}
