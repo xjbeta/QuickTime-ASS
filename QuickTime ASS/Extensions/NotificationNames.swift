bc3b5c048d651effe993bd723fe6d88275349dbe
xjbeta <xjbeta.tiny@gmail.com>
2021-01-21 16:57:00 +0800

    Notification.Name.

--- QuickTime ASS/Extensions/NotificationNames.swift
+++ QuickTime ASS/Extensions/NotificationNames.swift
@@ -9,5 +9,6 @@ import Cocoa
 
 extension Notification.Name {
     static let loadNewSubtilte = Notification.Name("com.xjbeta.QuickTime ASS.loadNewSubtilte")
-    
+    static let updatePlayState = Notification.Name("com.xjbeta.QuickTime ASS.updatePlayState")
+    static let updateTargeWindowState = Notification.Name("com.xjbeta.QuickTime ASS.updateTargeWindowState")
 }
