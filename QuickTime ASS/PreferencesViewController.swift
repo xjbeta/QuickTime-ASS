8d99b972e51d698d6a7d604f9f704b3c1364abf0
xjbeta <xjbeta.tiny@gmail.com>
2021-02-17 09:59:29 +0800

    PreferencesViewController.

---
+++ QuickTime ASS/PreferencesViewController.swift
@@ -0,0 +1,52 @@
+//
+//  PreferencesViewController.swift
+//  QuickTime ASS
+//
+//  Created by xjbeta on 2/15/21.
+//
+
+import Cocoa
+
+class PreferencesViewController: NSViewController {
+    
+    @IBOutlet var dailySlider: NSSlider!
+    @IBOutlet var dailyTextField: NSTextField!
+    
+    @IBOutlet var scaleSlider: NSSlider!
+    @IBOutlet var scaleTextField: NSTextField!
+    
+    @IBOutlet var positionSlider: NSSlider!
+    @IBOutlet var positionTextField: NSTextField!
+    
+    @IBAction func sliderAction(_ sender: NSSlider) {
+        switch sender {
+        case dailySlider:
+            let v = sender.doubleValue / 2
+            dailyTextField.stringValue = "\(v >= 0 ? "+" : "")\(v)s"
+        case scaleSlider:
+            var v = sender.doubleValue
+            if v > 1 {
+                v *= 2
+            }
+            
+            let s = String(format: "%.2f", v)
+            scaleTextField.stringValue = "\(s)x"
+        case positionSlider:
+            let v = sender.integerValue
+            positionTextField.stringValue = "\(v)"
+        default:
+            break
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        
+        
+        
+    }
+    
+}
